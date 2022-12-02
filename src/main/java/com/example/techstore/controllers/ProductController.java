30e939b9d293f80d5ac3c323165cebda84d2aee8
ekinbayri <ekinbayri@gmail.com>
2022-12-02 18:48:30 +0300

    Update ProductController.java

--- src/main/java/com/example/techstore/controllers/ProductController.java
+++ src/main/java/com/example/techstore/controllers/ProductController.java
@@ -3,8 +3,14 @@ package com.example.techstore.controllers;
 import com.example.techstore.entities.Product;
 import com.example.techstore.services.ProductService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +26,23 @@ public class ProductController {
     public Product saveOneProduct(@RequestBody Product product) {
         
         return productService.saveOneProduct(product);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteOneProduct(@PathVariable Long id) {
+        productService.deleteOneProduct(id);
+    }
+    @GetMapping(value = {"/{id}"})
+    public Product getOneProduct(@PathVariable(name = "id") Long id) {
+        Product product = productService.getOneProduct(id);
+         return product;  
+    }
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+    @PutMapping("{id}")
+    public Product updateOneProduct(@PathVariable Long id, @RequestBody Product product){
+        return productService.updateOneProduct(id, product);
     }
 
     
